b365c9da46c88223f7eabd747d742c49a2af4295
Aringoaway <a.konshina.job@gmail.com>
2022-04-15 18:12:52 +0200

    add useState for Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Categories({items}) {
+    const [activeItem, setActiveItem] = useState(null);
+
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+
     return(
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                {items.map((name, index) =>
-                    <li key={`${name}_${index}`}>{name}</li>
+                <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
+                {items && items.map((name, index) =>
+                    <li
+                        className={activeItem === index ? 'active' : ''}
+                        onClick={() => onSelectItem(index)}
+                        key={`${name}_${index}`}>
+                        {name}
+                    </li>
                 )}
             </ul>
         </div>
