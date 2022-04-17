d40a9f33ed0adb32e0d57aa40029cfd0f63f9428
Aringoaway <a.konshina.job@gmail.com>
2022-04-17 12:26:06 +0200

    add json and fetch in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import { Header } from './components';
-import { Home, Cart } from './pages';
+import {Routes, Route} from "react-router-dom";
+import {Header} from './components';
+import {Home, Cart} from './pages';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json')
+            .then((resp) => resp.json())
+            .then(json => {
+            setPizzas(json.pizzas);
+        });
+    }, []);
+
+    console.log(pizzas)
+
     return (
         <div className="wrapper">
             <Header/>
