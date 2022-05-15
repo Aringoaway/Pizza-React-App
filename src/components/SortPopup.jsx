07c85d4fc0d880e8986e81f4266c4369f4ddc789
Aringoaway <a.konshina.job@gmail.com>
2022-05-15 15:56:27 +0200

    refactor sortBy

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -54,7 +54,7 @@ const SortPopup = React.memo(function SortPopup({ items, onClickSortType, active
                         {items && items.map((obj, index) =>
                             <li
                                 className={activeSortType === obj.type ? 'active' : ''}
-                                onClick={() => onSelectItem(obj.type)}
+                                onClick={() => onSelectItem(obj)}
                                 key={`${obj.type}_${index}`}>
                                 {obj.name}
                             </li>
